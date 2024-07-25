3367bb4f4600a65cb30f774c3daa04a331636422
Kim Corson <kimmc67b@gmail.com>
2024-07-25 09:42:44 -0500

    continued

--- components/EditorContent.tsx
+++ components/EditorContent.tsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { StyleSheet, View, Image } from 'react-native';
+import { useContext, useState } from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
 import { ImageCtx } from './ImageSelection/ImageCtx';
 
 import PhotoSelectTool from './ImageSelection/PhotoSelectTool';
@@ -23,59 +23,73 @@ const EditorContent = () => {
     const { drawingData } = useContext(DrawCtx);
     const { stickers } = useContext(StickerCtx);
 
+    const [ stickerMenuToggle, setStickerMenuToggle ] = useState<boolean>(false);
+
+    // call back to be handled as prop value upon using the stickerTool comp
+    const handleToggleStickerMenu = () => {
+      setStickerMenuToggle(!stickerMenuToggle);
+      console.log("menu toggle", !stickerMenuToggle);
+    }
+
 return (
+  <View style={styles.screenContainer}>
+
+    <Image style={styles.headerImg} // later use screenOptions?
+      source={require('../assets/images/ElementalEditorBanner.png')}
+    />
+
     <View style={styles.screenContainer}>
 
-        <Image style={styles.headerImg} // later use screenOptions?
-          source={require('../assets/images/ElementalEditorBanner.png')}
-        />
+      <View style={styles.canvasContainer}>
 
-        <View style={styles.screenContainer}>
-
-          <View style={styles.canvasContainer}>
-
-{/* PHOTOS */}
-            {imagesData.length > 0 &&
-              <View style={styles.canvas}>
-                {imagesData.map((imageCtx, index) => (
-                  <View>
-                    {/* image will need to have set default randomized scattered location data, and if user moves it, that will be updated location data top/left 
-                    Thinking that I will make an Image object and it will contain an imageInfo and top, left values. if the imageInfo in the particular object matches 
-                    this index here it will use the corresponding top and left data. Can't jsut add it to image info bc that has known specific data */}
-                    <Image 
-                      key={ index }
-                      source={{ uri: imageCtx.imageInfo.uri }}
-                      style={{ 
-                        width: 100, height: 100, 
-                        flexDirection: 'column', // idk why but this helps with the scattering
-                        top: `${imageCtx.top}%`,
-                        left: `${imageCtx.left}%` }} 
-                  />  
-                  </View>
-                ))}
+        {/* PHOTOS */}
+        {imagesData.length > 0 &&
+          <View style={styles.canvas}>
+            {imagesData.map((imageCtx, index) => (
+              <View>
+                {/* image will need to have set default randomized scattered location data, and if user moves it, that will be updated location data top/left 
+                Thinking that I will make an Image object and it will contain an imageInfo and top, left values. if the imageInfo in the particular object matches 
+                this index here it will use the corresponding top and left data. Can't jsut add it to image info bc that has known specific data */}
+                <Image 
+                  key={ index }
+                  source={{ uri: imageCtx.imageInfo.uri }}
+                  style={{ 
+                    width: 100, height: 100, 
+                    flexDirection: 'column', // idk why but this helps with the scattering
+                    top: `${imageCtx.top}%`,
+                    left: `${imageCtx.left}%` }} 
+              />  
               </View>
-            }
-
-{/* STICKERS */}
-<View>
-  {stickers.map((stickerCtx, index) => ( // review everything here
-    <View>
-        <Image
-          key={ index }
-          source={{ uri: stickerCtx.sticker.uri }}
-          style={{
-            width: 50, height: 50, 
-            flexDirection: 'column', // idk why but this helps with the scattering
-            top: `${stickerCtx.top}%`,
-            left: `${stickerCtx.left}%` }} 
-            />
-    </View>
-  ))}
-</View>
-
+            ))}
           </View>
+        }
 
-          <View style={styles.editorTools}>
+        {/* STICKERS - where they show up after selection, this is not the sticker toolbar*/}
+        <View>
+          {stickers.map((stickerCtx, index) => ( // review everything here
+            <View>
+                <Image
+                  key={ index }
+                  source={{ uri: stickerCtx.sticker.uri }}
+                  style={{
+                    width: 50, height: 50, 
+                    flexDirection: 'column', // idk why but this helps with the scattering
+                    top: `${stickerCtx.top}%`,
+                    left: `${stickerCtx.left}%` }} 
+                    />
+            </View>
+          ))}
+        </View>
+      </View>
+
+      <View style={styles.editorTools}>
+
+        {/* STICKER SELECTION MENU either this or editor toolbar*/}
+        {stickerMenuToggle ? (
+          <View>
+            <Text>STICKER OPTIONS WILL COME FROM A FILE DIR OF PHOTOS</Text>
+          </View>
+          ) : (
             <StyledIconContainer>
 
               <PhotoSelectTool>
@@ -90,15 +104,16 @@ return (
 
               <Feather name='layout' size={35}/>
 
-              <StickerTool>
+              <StickerTool menuToggle={handleToggleStickerMenu}>
                 <Octicons name='smiley' size={35}/>
               </StickerTool>
                             
             </StyledIconContainer>
+            )}
 
-            </View>
         </View>
       </View>
+    </View>
   );
 }
       
@@ -138,6 +153,17 @@ const styles = StyleSheet.create({
       width: '100%', // 100% of parent which is canvas container
     },
     editorTools: {
+      display: 'flex',
+      flexWrap: 'wrap',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      gap: 50,
+      rowGap: 15,
+      top: '130%',
+      borderTopWidth: 1,
+      padding: 15,
+    },
+    stickerMenu: {
       display: 'flex',
       flexWrap: 'wrap',
       flexDirection: 'row',
--- components/Stickers/StickerTool.tsx
+++ components/Stickers/StickerTool.tsx
@@ -5,13 +5,16 @@ import AddStickerUtil from './AddStickerUtil';
 
 interface StickerToolProps {
   children?: React.ReactNode;
+  menuToggle: () => void;
 }
 
-const StickerTool: React.FC<StickerToolProps> = ({children}) => {
+const StickerTool: React.FC<StickerToolProps> = ({children, menuToggle}) => {
   const [toggleStickerSelect, setToggleStickerSelect] = useState<boolean>(false);
 
   const handleSelectStickers = () => {
     setToggleStickerSelect(!toggleStickerSelect);
+    menuToggle(); // menu toggle comes in as false, so by ! it sets it to true same as the toggle for sticker select, then turns to false when icon is clicked again
+    console.log("handle select stickers", !toggleStickerSelect);
   };
 
   return (
