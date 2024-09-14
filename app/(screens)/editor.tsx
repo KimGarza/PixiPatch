fe067c806feaed2e6ee8acfe0ced99add9f8cd82
Kim Corson <kimmc67b@gmail.com>
2024-09-14 12:55:15 -0500

    implemented better code linting

--- app/(screens)/editor.tsx
+++ app/(screens)/editor.tsx
@@ -20,85 +20,80 @@ import GlobalDimensions from '@/components/dimensions/globalDimensions';
 
 const { width, height, canvasHeight, headerHeight } = GlobalDimensions();
 
-const EditorScreen = () => { 
+const EditorScreen = () => {
   // contexts
   const { background } = useContext(BackgroundCtx);
   const { images, stickers, drawings } = useItemCtx();
   // menus
-  const [ stickerMenuToggle, setStickerMenuToggle ] = useState<boolean>(false);
-  const [ backgroundMenuToggle, setBackgroundMenuToggle ] = useState<boolean>(false);
-  const [ drawMenuToggle, setDrawMenuToggle ] = useState<boolean>(false);
+  const [stickerMenuToggle, setStickerMenuToggle] = useState<boolean>(false);
+  const [backgroundMenuToggle, setBackgroundMenuToggle] =
+    useState<boolean>(false);
+  const [drawMenuToggle, setDrawMenuToggle] = useState<boolean>(false);
   // misc
   const viewRef = useRef(null); // used to capture the canvas container View elemenet
 
-  useEffect(() => {
-  }, [images, drawings, stickers])
+  useEffect(() => {}, [images, drawings, stickers]);
 
   // Menu Callbacks - allows for conditional displaying of menus based on opened or closed
   const handleToggleStickerMenuCallback = () => {
-    setStickerMenuToggle(!stickerMenuToggle);
-  }
+        setStickerMenuToggle(!stickerMenuToggle);
+  };
 
   const handleToggleBackgroundMenuCallback = () => {
     setBackgroundMenuToggle(!backgroundMenuToggle);
-  }
+  };
 
   const handleToggleDrawMenuCallback = () => {
     setDrawMenuToggle(!drawMenuToggle);
-  }
+  };
 
-return (
-  <View style={styles.screenContainer}>
-
-    {/* header */}
-    <View style={styles.headerNav}>
-      <Image
-        style={styles.headerImg}
-        source={require('../../assets/images/ElementalEditorBanner.png')}
-      />
-        <HomeButton/>
+  return (
+    <View style={styles.screenContainer}>
+      {/* header */}
+      <View style={styles.headerNav}>
+        <Image
+          style={styles.headerImg}
+          source={require('../../assets/images/ElementalEditorBanner.png')}
+        />
+        <HomeButton />
         {/* viewRef is ref of canvas container 'View' element. Passing into SaveWorkButton bc this contains the user's beautiful creation that will be saved! */}
-        <SaveButtonAndMenu viewRef={viewRef.current}/>
-    </View>
+        <SaveButtonAndMenu viewRef={viewRef.current} />
+      </View>
 
-    {/* main canvas */}
-    <View style={styles.canvasContainer} ref={viewRef} collapsable={false} >
-
-      <ImageBackground source={background} style={styles.imageBackground}>
-
-        <View style={styles.canvas} >
-          
+      {/* main canvas */}
+      <View style={styles.canvasContainer} ref={viewRef} collapsable={false}>
+        <ImageBackground source={background} style={styles.imageBackground}>
+          <View style={styles.canvas}>
             {/* Stickers */}
-            <ViewStickers stickers={stickers}/>
+            <ViewStickers stickers={stickers} />
 
             {/* Drawing */}
-            <ViewDrawings drawings={drawings}/>
+            <ViewDrawings drawings={drawings} />
 
             {/* Pictures */}
-            <ViewImages images={images}/> 
-
-        </View>
-      </ImageBackground>
-    </View>
+            <ViewImages images={images} />
+          </View>
+        </ImageBackground>
+      </View>
 
       {/* Bottom Toolbar - alternates between primary editing tools and menus for active in-use tool */}
-      { stickerMenuToggle ? (
-        <StickerMenu menuToggle={handleToggleStickerMenuCallback}/>
+      {stickerMenuToggle ? (
+        <StickerMenu menuToggle={handleToggleStickerMenuCallback} />
       ) : backgroundMenuToggle ? (
-        <BackgroundMenu menuToggle={handleToggleBackgroundMenuCallback}/>
+        <BackgroundMenu menuToggle={handleToggleBackgroundMenuCallback} />
       ) : (
-      // primary tools
-      <View style={styles.primaryTools}>
-        <ViewEditorTools
-          backgroundMenuToggle={handleToggleBackgroundMenuCallback}
-          drawMenuToggle={handleToggleDrawMenuCallback}
-          stickerMenuToggle={handleToggleStickerMenuCallback}
-        />
-      </View>
+        // primary tools
+        <View style={styles.primaryTools}>
+          <ViewEditorTools
+            backgroundMenuToggle={handleToggleBackgroundMenuCallback}
+            drawMenuToggle={handleToggleDrawMenuCallback}
+            stickerMenuToggle={handleToggleStickerMenuCallback}
+          />
+        </View>
       )}
     </View>
   );
-}
+};
 
 export default EditorScreen;
 
@@ -108,21 +103,23 @@ const styles = StyleSheet.create({
   },
   headerNav: {
     zIndex: 9999999,
-    height: headerHeight
+    height: headerHeight,
   },
   headerImg: {
     width: '100%',
-  },  
+  },
   canvasContainer: {
     height: canvasHeight,
     width: width,
   },
   canvas: {
     overflow: 'hidden',
-    height: '100%', width: '100%',
+    height: '100%',
+    width: '100%',
   },
   imageBackground: {
-    width: '100%', height: '100%',
+    width: '100%',
+    height: '100%',
     zIndex: 1,
   },
   primaryTools: {
@@ -133,6 +130,7 @@ const styles = StyleSheet.create({
     gap: 30,
     zIndex: 9999999,
     padding: 15,
-    borderTopWidth: .6, borderColor: 'black',
+    borderTopWidth: 0.6,
+    borderColor: 'black',
   },
 });
