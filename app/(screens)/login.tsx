180936e106717032aa0da83da7e5be1fcf8f8e59
Kim Corson <kimmc67b@gmail.com>
2024-09-29 09:45:23 -0500

    testing -A

--- app/(screens)/login.tsx
+++ app/(screens)/login.tsx
@@ -3,6 +3,7 @@ import { View, Text } from "react-native";
 export default function LoginSignUpScreen() {
     return (
         <View>
+            
             <Text>Login Screen</Text>
         </View>
     );
